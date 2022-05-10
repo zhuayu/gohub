1493c77102cc3826f8465aac21677b311c6be1d9
Jax Chu <294242589@qq.com>
2022-05-10 14:14:55 +0800

    feat: bootstrap router

--- main.go
+++ main.go
@@ -1,38 +1,24 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+	"gohub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	r := gin.New()
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
-	r.Use(gin.Logger(), gin.Recovery())
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-	r.Run(":8001")
+	// 运行服务
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
