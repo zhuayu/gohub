93a3bea1ed47cdca63b8ed5edceaa7a2f37795ca
Jax Chu <294242589@qq.com>
2022-05-11 21:36:43 +0800

    feat: links

--- routes/api.go
+++ routes/api.go
@@ -79,6 +79,12 @@ func RegisterAPIRoutes(r *gin.Engine) {
 				tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
 				tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
 			}
+
+			lsc := new(controllers.LinksController)
+			linksGroup := v1.Group("/links")
+			{
+				linksGroup.GET("", lsc.Index)
+			}
 		}
 	}
 }
